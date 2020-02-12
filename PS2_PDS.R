fc34fa0caa9fed1d5e820f7d3f611b4918389342
Jin <60522038+dahjinkim@users.noreply.github.com>
2020-02-12 14:57:57 -0600

    Update PS2_PDS.R

--- PS2_PDS.R
+++ PS2_PDS.R
@@ -6,16 +6,70 @@ for(i in 1:7){
   print(i^3)
 }
 
+
 #2
-set.seed(14)
-count <- NULL
+count <- 0
 for(i in 1:1000){
   roll <- sample(1:6, 2, replace = T)
   sum.roll <- sum(roll)
-  if (sum.roll >= 8 & sum.roll <=12) {
+  if (i == 1 & sum.roll >= 8) {
+    print(roll)
+    break
+  } else if (i != 1 & (roll[1] == 2 | roll[2] == 2 | roll[1] == 6 | roll[2] == 6)) {
     print(i)
+    print(roll)
+    count <- c(count, i)
     break
   }
-  count <- c(count, i)
-  print(paste("The average trial is", mean(count)))
 }
+
+
+#3
+#reading in the data
+library(readr)
+GSS <- read.csv("http://politicaldatascience.com/PDS/Problem%20Sets/Problem%20Set%202/
+GSS-data.csv")
+
+#creating a function that takes in one of "Trump", "Clinton", and "Other"
+#and return the number of participants who voted for each of the inputs.
+vote.choice <- function(x){
+  if(x == "Trump"){
+    return(sum(GSS$pres16=="Trump"))
+  } else if (x=="Clinton"){
+    return(sum(GSS$pres16=="Clinton"))
+  } else if (x== "Other"){
+    return(sum(GSS$pres16 != "Clinton" & GSS$pres16 != "Trump"))
+  } else {
+    return(NULL)
+  }
+}
+#test to see if it works
+vote.choice("Trump")
+vote.choice("Clinton")
+vote.choice("Other")
+
+#now edit the function so that something else is entered,
+#the function returns the message "Please enter ..."
+vote.choice <- function(x){
+  if(x == "Trump"){
+    return(sum(GSS$pres16=="Trump"))
+  } else if (x=="Clinton"){
+    return(sum(GSS$pres16=="Clinton"))
+  } else if (x== "Other"){
+    return(sum(GSS$pres16 != "Clinton" & GSS$pres16 != "Trump"))
+  } else {
+    return(print("Please enter either 'Trump' 'Clinton' or 'Other' into the function to return a valid response."))
+  }
+}
+#test to see if it works
+Clington <- c(1, 2, 3)
+vote.choice(1)
+vote.choice("Clington")
+vote.choice(Clington)
+
+
+#4.
+#installing packages
+install.packages("fivethirtyeight")
+library(fivethirtyeight)
+
